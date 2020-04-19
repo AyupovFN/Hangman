2e44cf33546a86c5899c661436d9cb3c7098513c
AyupovFN <kapterka@bk.ru>
2020-04-19 17:14:25 +0300

    initial commit

---
+++ lib/game.rb
@@ -0,0 +1,66 @@
+
+class Game
+  MAX_ERRORS = 7
+  def initialize(word)
+    @letters = word.chars
+    @normalized_letters = normalized_letters(word.chars)
+    @user_guesses = normalized_letters([])
+  end
+
+  def errors
+    @user_guesses - @normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    MAX_ERRORS - errors_made
+  end
+
+  def letters_to_guess
+    result = @letters.map do |letter|
+      if @user_guesses.include?(normalize_letter(letter))
+        letter
+      else
+        nil
+      end
+    end
+  end
+
+  def normalized_letters(word_for_normalize)
+    result = word_for_normalize.map do |letter|
+      normalize_letter(letter)
+    end
+  end
+
+  def normalize_letter(letter)
+    return 'Е' if letter == 'Ё'
+    return 'И' if letter == 'Й'
+
+    letter
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    if !over? && !@user_guesses.include?(normalize_letter(letter))
+      @user_guesses << normalize_letter(letter)
+    end
+  end
+
+  def word
+    @letters.join
+  end
+
+  def won?
+    (@normalized_letters - @user_guesses).empty?
+  end
+
+  def lost?
+    errors_allowed.zero?
+  end
+end
---
+++ main.rb
@@ -0,0 +1,25 @@
+require_relative 'lib/console_interface'
+require_relative 'lib/game'
+
+
+puts "Всем привет!"
+
+word = File.readlines("#{__dir__}/data/words.txt", chomp: true).sample
+
+
+game = Game.new(word.chomp)
+console_interface = ConsoleInterface.new(game)
+
+
+until game.over? # пока не закончилась игра
+  # вывести очередное состояние игры
+  console_interface.print_out
+  # спросить очередную букву
+  letter = console_interface.get_input
+  # обновить сосотояние игры
+  game.play!(letter)
+end
+
+console_interface.print_out
+
+
