9ef46c1db7ef407cc915707826f082889b3fc349
AyupovFN <kapterka@bk.ru>
2020-04-19 22:03:45 +0300

    add gem colorize

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -5,23 +5,25 @@ class ConsoleInterface
       Dir[__dir__+'/../data/figures/*.txt'].
       sort.map { |file_name| File.read(file_name) }
 
+  COLOR_LATTER = [:green, :blue, :white, :red, :yellow]
+
   def initialize(game)
     @game = game
   end
 
   def print_out
     puts <<~END
-      Слово: #{word_to_show}
-      #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
-      У вас осталось ошибок: #{@game.errors_allowed}
+      Слово: #{word_to_show.colorize(COLOR_LATTER.sample)}
+      #{figure.colorize(:yellow)}
+      #{"Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)}
+      #{"У вас осталось ошибок:".colorize(:green)} #{@game.errors_allowed.to_s.black.on_yellow.blink}
 
     END
 
     if @game.won?
-      puts "Поздравляю вы выграли!"
+      puts "#{"Поздравляю вы выграли!".colorize(COLOR_LATTER.sample)}"
     elsif @game.lost?
-      puts "вы проиграли, загадоное слово: #{@game.words}"
+      puts "вы проиграли, загадоное слово: #{@game.word}"
     end
   end
 
@@ -31,7 +33,7 @@ class ConsoleInterface
 
   def word_to_show
     result =
-      @game.letters_to_guesses.map do |letter|
+      @game.letters_to_guess.map do |letter|
         if letter.nil?
          "__"
         else
@@ -47,7 +49,7 @@ class ConsoleInterface
   end
 
   def get_input
-    print "Введите следующую букву: "
+    print "#{"Введите следующую букву:".colorize(:blue)}"
     letter = gets[0].capitalize
     return letter
   end
